61a891ae00a01070a68ff41835c30b12779d5088
lhas <luizhrqas@gmail.com>
2017-02-07 01:43:12 -0200

    added contact route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'events#index'
 
+  root to: 'events#index'
   get '/eventos/:id', to: 'events#show', as: 'event'
+  get '/contato', to: 'contacts#new', as: 'contact'
 end
