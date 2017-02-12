86772f9e213034ba28a9c6421ab06fc1f0d718dd
lhas <luizhrqas@gmail.com>
2017-02-12 10:39:41 -0200

    [chore] suggest event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,16 @@ class EventsController < ApplicationController
     @event = Events::DisplayService.show(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    Events::CreateService.send(event_params)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :event_date, :more_info, :price, :description, :city, :state)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   root to: 'events#index'
+  get '/eventos/sugerir', to: 'events#new', as: 'new_event'
+  post '/eventos/sugerir', to: 'events#create', as: 'create_event'
   get '/eventos/:id', to: 'events#show', as: 'event'
+
   get '/contato(/:sent)', to: 'contacts#new', as: 'contact'
   post '/contato', to: 'contacts#create', as: 'create_contact'
 end
